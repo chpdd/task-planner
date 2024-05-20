53a1b4400f669df716a3b3cc5329186ffb6937e2
Denis <jhj@>
2024-05-21 00:29:01 +0300

    Little update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class InvalidTaskError(Exception):
 
 class Task:
     # work_hours in day
-    __work_hours = timedelta(hours=4)
+    __day_work_hours = timedelta(hours=4)
 
     def __init__(self, name: str, **kwargs):
         # if (kwargs.get("deadline", None) is None
@@ -29,23 +29,62 @@ class Task:
         self.__interest = int(kwargs.get("interest", 5))
 
         # lead time in hours(default=4 hours)
-        if "lead_time" in kwargs:
-            self.__lead_time = timedelta(hours=int(kwargs["lead_time"]))
+        if "task_work_hours" in kwargs:
+            self.__task_work_hours = timedelta(hours=int(kwargs["task_work_hours"]))
         else:
-            self.__lead_time = self.__work_hours
+            self.__task_work_hours = self.__day_work_hours
 
         # Task must do(default=True)
         self.__must_do = False if kwargs.get("must_do", True) == "False" else True
 
     def __str__(self):
         return (f"Task name: {self.__name}, deadline: {self.__deadline}, "
-                f"interest: {self.__interest}/10, lead_time: {self.__lead_time}, "
+                f"interest: {self.__interest}/10, task_work_hours: {self.__task_work_hours}, "
                 f"must_do: {self.__must_do} ")
 
     def __repr__(self):
         return (f"Task(name={self.__name}, deadline={self.__deadline}, "
-                f"interest={self.__interest}, lead_time={self.__lead_time}, "
+                f"interest={self.__interest}, task_work_hours={self.__task_work_hours}, "
                 f"must_do={self.__must_do})")
+
+    @property
+    def interest(self):
+        return self.__interest
+
+    @interest.setter
+    def interest(self, interest):
+        self.interest = interest
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def deadline(self):
+        return self.__deadline
+
+    @deadline.setter
+    def deadline(self, deadline):
+        self.__deadline = deadline
+
+
+def print_tasks_list(tasks_list: list[Task], *attrs):
+    for tsk in tasks_list:
+        for attr in attrs:
+            if attr == "deadline":
+                print(f"{attr}: {getattr(tsk, attr, "No attr").date()}", end=", ")
+            else:
+                print(f"{attr}: {getattr(tsk, attr, "No attr")}", end=", ")
+            # print(tsk.func())
+        print()
+
+
+def sort_by_interest(tasks_list: [Task]):
+    return sorted(tasks_list, key=lambda tsk: tsk.interest, reverse=True)
 
 
 if __name__ == "__main__":
@@ -68,7 +107,10 @@ if __name__ == "__main__":
                     kwargs[key.strip()] = arg.strip()
             tasks.append(Task(name, **kwargs))
             # print(name, kwargs)
-    for task in tasks:
-        print(task)
-        # print(task._Task__name, task._Task__deadline, task._Task__interest,
-        # task._Task__lead_time, task._Task__must_do)
+
+    attrs = ["name", "interest", "deadline"]
+    print_tasks_list(sort_by_interest(tasks), *attrs)
+
+    # better first
+    if allocation_type == "better_first":
+        pass
