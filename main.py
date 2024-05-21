8712aebbfd07285b116972cfb7d49d3c1be44410
Correct Name <chupilko.dd@gmail.com>
2024-05-21 09:56:25 +0300

    Working with new functions

--- main.py
+++ main.py
@@ -29,22 +29,22 @@ class Task:
         self.__interest = int(kwargs.get("interest", 5))
 
         # lead time in hours(default=4 hours)
-        if "task_work_hours" in kwargs:
-            self.__task_work_hours = timedelta(hours=int(kwargs["task_work_hours"]))
+        if "work_hours" in kwargs:
+            self.__work_hours = timedelta(hours=int(kwargs["work_hours"]))
         else:
-            self.__task_work_hours = self.__day_work_hours
+            self.__work_hours = self.__day_work_hours
 
         # Task must do(default=True)
-        self.__must_do = False if kwargs.get("must_do", True) == "False" else True
+        self.__must_do = False if kwargs.get("must_do", None) == "False" else True
 
     def __str__(self):
         return (f"Task name: {self.__name}, deadline: {self.__deadline}, "
-                f"interest: {self.__interest}/10, task_work_hours: {self.__task_work_hours}, "
+                f"interest: {self.__interest}/10, work_hours: {self.__work_hours}, "
                 f"must_do: {self.__must_do} ")
 
     def __repr__(self):
         return (f"Task(name={self.__name}, deadline={self.__deadline}, "
-                f"interest={self.__interest}, task_work_hours={self.__task_work_hours}, "
+                f"interest={self.__interest}, work_hours={self.__work_hours}, "
                 f"must_do={self.__must_do})")
 
     @property
@@ -71,46 +71,94 @@ class Task:
     def deadline(self, deadline):
         self.__deadline = deadline
 
+    @property
+    def must_do(self):
+        return self.__must_do
 
-def print_tasks_list(tasks_list: list[Task], *attrs):
-    for tsk in tasks_list:
+    @must_do.setter
+    def must_do(self, must_do):
+        self.__must_do = must_do
+
+
+class Day:
+    def __init__(self, date, **kwargs):
+        self.__date = date
+        # self.__weekend = True if kwargs.get("weekend", None) == "True" else: False
+        self.__work_hours = timedelta(hours=int(kwargs.get("work_hours", 4)))
+
+    def __repr__(self):
+        return f"Day(date={self.__date}, day_work_hours={self.__work_hours})"
+
+    def __str__(self):
+        return f"Day date: {self.__date}, day_work_hours={self.__work_hours}"
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, date):
+        self.__date = date
+
+    @property
+    def work_hours(self):
+        return self.__work_hours
+
+    @work_hours.setter
+    def work_hours(self, work_hours):
+        self.__work_hours = work_hours
+
+
+def print_class_objs(input_list: [], *attrs):
+    for elem in input_list:
         for attr in attrs:
-            if attr == "deadline":
-                print(f"{attr}: {getattr(tsk, attr, "No attr").date()}", end=", ")
-            else:
-                print(f"{attr}: {getattr(tsk, attr, "No attr")}", end=", ")
-            # print(tsk.func())
+            result = getattr(elem, attr, "No attr")
+            if isinstance(result, datetime):
+                result = result.date()
+            print(f"{attr}={result}", end=", ")
         print()
 
 
-def sort_by_interest(tasks_list: [Task]):
-    return sorted(tasks_list, key=lambda tsk: tsk.interest, reverse=True)
+def read_class_objs(file_name: str, class_init, * pos_attr_names) -> []:
+    result = []
+    with open(file_name, 'r') as input_file:
+        lines = input_file.readlines()
+        for line in lines:
+            attrs = line.strip().split(",")
+            kw_attrs = dict()
+            pos_attrs = []
+            for i in range(len(attrs)):
+                attr_str = attrs[i].strip()
+                if i < len(pos_attr_names):
+                    pos_attrs.append(attr_str)
+                elif ":" in attr_str:
+                    key, val = attr_str.split(":")
+                    kw_attrs[key.strip()] = val.strip()
+                elif "=" in attr_str:
+                    key, val = attr_str.split("=")
+                    kw_attrs[key.strip()] = val.strip()
+            result.append(class_init(*pos_attrs, **kw_attrs))
+    return result
 
 
 if __name__ == "__main__":
     # types: better_first, better_last, mixed
     allocation_type = "better_first"
     weekends = []
-    tasks = []
-    with open('input.txt', 'r') as f_input:
-        lines = f_input.readlines()
-        for line in lines:
-            strings = line.strip().split(",")
-            kwargs = dict()
-            name = strings[0].strip()
-            for string in strings[1:]:
-                if ":" in string:
-                    key, arg = string.strip().split(":")
-                    kwargs[key.strip()] = arg.strip()
-                elif "=" in string:
-                    key, arg = string.strip().split("=")
-                    kwargs[key.strip()] = arg.strip()
-            tasks.append(Task(name, **kwargs))
-            # print(name, kwargs)
 
-    attrs = ["name", "interest", "deadline"]
-    print_tasks_list(sort_by_interest(tasks), *attrs)
+    # get class lists
+    task_pos_attr_names = ["name"]
+    tasks = read_class_objs('tasks.txt', Task, task_pos_attr_names)
+    day_pos_attr_names = ["date"]
+    days = read_class_objs('days.txt', Day, day_pos_attr_names)
+
+    # print class lists
+    task_attrs = ["name", "interest", "deadline"]
+    print_class_objs(sorted(tasks, key=lambda tsk: tsk.interest, reverse=True),
+                     *task_attrs)
+    day_attrs = ["date", "work_hours"]
+    print_class_objs(sorted(days, key=lambda d: d.date), *day_attrs)
 
     # better first
     if allocation_type == "better_first":
-        pass
+        tasks.sort(key=lambda tsk: (tsk.must_do, tsk.interest))
